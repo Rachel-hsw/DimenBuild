2e96cab12284e7a9e093057bf2bb6ee801fed8d1
rachel <rachelhuang@tiidian.com>
2019-02-16 17:21:41 +0800

    Initial commit

---
+++ app/src/main/java/com/example/rachel/myapplication/Diamen.java
@@ -0,0 +1,17 @@
+package com.example.rachel.myapplication;
+
+import com.mango.dimen.DimensBuild;
+
+/**
+ * Created by Rachel on 2019/2/15.
+ */
+
+public class Diamen {
+    public static void main(String[] args){
+
+        //下面两种方法任选其一，具体参数信息见代码
+//        DimensBuild.buildDP();
+        DimensBuild2.buildPX();
+
+    }
+}
