be59581cfdb44bd45aa0d6f04cc466b2739a6d24
rachel <rachelhuang@tiidian.com>
2019-02-21 14:04:31 +0800

    增加修改源码方式

--- app/src/main/java/com/example/rachel/myapplication/Diamen.java
+++ app/src/main/java/com/example/rachel/myapplication/Diamen.java
@@ -1,6 +1,7 @@
 package com.example.rachel.myapplication;
 
-import com.mango.dimen.DimensBuild;
+//import com.mango.dimen.DimensBuild;
+import com.mango.dimen.DimensBuild3;
 
 /**
  * Created by Rachel on 2019/2/15.
@@ -10,8 +11,12 @@ public class Diamen {
     public static void main(String[] args){
 
         //下面两种方法任选其一，具体参数信息见代码
-//        DimensBuild.buildDP();
+        //也可以自己写
         DimensBuild2.buildPX();
+        //可以添加依赖后直接调用（或者作为jar放到libs目录下调用）这种方式不允许修改源码
+//        DimensBuild.buildDP();
+        //也可以源码下载下来,作为library库导入到AS当中（File->new->new model->导入之后，为主model添加对这个库的依赖->project structure->dependencies）
+        DimensBuild3.buildPX();
 
     }
 }
